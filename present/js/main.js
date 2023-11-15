c0134a4c3772d0b37a466387a71e8fa2d37b8204
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-11-15 10:16:14 -0500

    s

---
+++ present/js/main.js
@@ -0,0 +1,10 @@
+import '..style/style.css'
+
+const ariel =  [
+
+
+     <h3 class="card-one-title">Cinderella</h3>
+        <img src="https://www.blog44.ca/ashac/files/2021/05/BFF29F92-7243-44D7-AFBC-12E888A81982.jpeg" alt="Cinderella" class="card-one-img">
+        <h4 class="card-one-desc">Cinderella and Stepfamily</h4>
+]
+
