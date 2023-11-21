92bf09d5ba87a7ab23021449cba071984d7116e3
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-11-21 10:16:51 -0500

    d

--- present/js/princesses.js
+++ present/js/princesses.js
@@ -4,39 +4,54 @@ export {snowwhitesevendwarves}
 const littlemermaid = [
     {
         Name: " Ariel",
-        Image: 'https://www.toysruschina.com/dw/image/v2/BDGJ_PRD/on/demandware.static/-/Sites-master-catalog-toysrus/default/dw495d06d8/e/d/6/b/ed6ba2a6da1852f0535878237dfaf27d488f35f1_110960_i3.jpg?sw=500&sh=500&q=75',
-        Price: "$22.90"
+        Image: '/Ariel.jpg',
+        Price: "$22.90",
     },  
     {
         Name: "Ursula",
-        Image: '',
-       Price: 10.90,
+        Image: '/Urusla.jpg',
+       Price: "10.90",
     },
-    {
-        Name: "Sebastion",
-        Image: '',
-        Price: 12.48,
-    },
-    {
-        Name: "Flounder",
-        Image: '',
-        Price: 59.34,
-    },
-    {
-        Name: "Prince Eric",
-        Image: '',
-         Price: 22.77,
-    },
-    {
-        Name: "Max the Dog",
-        Image: '',
-         Price: 54.68,
-    },
+     {Name: "Max the Dog",
+    Image: '/MaxtheDog.jpg',
+   Price: "10.90", },
+  
+   {Name: "Prince Eric",
+    Image: '/PrinceEric.jpg',
+   Price: "10.90" ,},
+
+   {Name: "Lobster",
+    Image: "/SebastianLoster.jpg",
+   Price: "10.90" ,},
+
     ];
+    console.log (littlemermaid)
 
-    const snowwhitesevendwarves =[
-
+    const snowwhitesevendwarves = [
+        {
+            Name: " Snow",
+            Image: 'https://m.media-amazon.com/images/I/91Wx8zpDbhL.jpg',
+            Price: "$2.90"
+        },  
+        {
+            Name: "Prince",
+            Image: 'https://static.wikia.nocookie.net/princessdisney/images/a/ab/Florian.png/revision/latest?cb=20130709121636',
+           Price: "108.90",
+        },
+    
     ]
+    console.log (snowwhitesevendwarves)
+
+    const DOMSelectors = {
+        first:document.querySelector(".first"), 
+        second:document.querySelector(".second"),
+        third:document.querySelector(".third"),
+        fourth:document.querySelector(".fourth"),
+        fifth:document.querySelector(".fifth"),}
+    
+        DOMSelectors.first.addEventListener("click" ,function (event) { 
+            event.preventDefault();
+            console.log (littlemermaid); })
     
 
 
