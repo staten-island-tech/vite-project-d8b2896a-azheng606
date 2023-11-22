ffd45b697f5b70801bad635a4255866828128d54
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-11-22 09:55:18 -0500

    h

--- present/js/main.js
+++ present/js/main.js
@@ -5,3 +5,14 @@ import {snowwhitesevendwarves} from './princesses'
 console.log(littlemermaid);
 console.log (snowwhitesevendwarves)
 
+document.querySelector(".change").addEventListener("click", function () {
+    if (document.body.classList.contains("good")){
+        document.body.classList.add("bad")
+        document.body.classList.remove ("good");
+     } else {
+            document.body.classList.add("good")
+            document.body.classList.remove("bad")
+        }
+
+})
+
