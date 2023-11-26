a55cfb460c9c2e034ccd6044e872c21ac74eae9e
azheng606 <azheng606@gmail.com>
2023-11-26 14:13:09 -0500

    a

--- present/js/main.js
+++ present/js/main.js
@@ -6,13 +6,12 @@ console.log(littlemermaid);
 console.log (snowwhitesevendwarves)
 
 document.querySelector(".change").addEventListener("click", function () {
-    if (document.body.classList.contains("good")){
+ if (document.body.classList.contains("good")){
         document.body.classList.add("bad")
         document.body.classList.remove ("good");
      } else {
             document.body.classList.add("good")
             document.body.classList.remove("bad")
-        }
-
+        } 
 })
 
