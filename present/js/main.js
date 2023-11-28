d744ca3745ec0d93dab92e34d06e64fe8a3582e6
azheng606 <144460945+azheng606@users.noreply.github.com>
2023-11-28 10:16:06 -0500

    pp

--- present/js/main.js
+++ present/js/main.js
@@ -5,18 +5,8 @@ console.log(array);
 console.log (DOMSelectors)
 
 
-    DOMSelectors.change.addEventListener("click", function (event) {
-    event.preventDefault();
- if (document.body.classList.contains("good")){
-        document.body.classList.add("bad")
-        document.body.classList.remove ("good");
-     } else {
-            document.body.classList.add("good")
-            document.body.classList.remove("bad")
-        } 
-})
-
-/* DOMSelectors.first.addEventListener("click", function idea (event) { 
+  
+/*  DOMSelectors.first.addEventListener("click", function idea (event) { 
     event.preventDefault();
     clearfields (event)
     const ar = array.filter((array)=> array.Title === "Little Mermaid")
@@ -26,8 +16,8 @@ console.log (DOMSelectors)
     <img src= ${ar.Image} class="card-img">
         <h3 class="card-desc"> ${ar.Price}</h3>
     </div>`));
-})
-
+}) */
+/*
 DOMSelectors.second.addEventListener("mousedown", function idea (twice) { 
     clearfields (twice)
     const arr = array.filter((array)=> array.Title === "Snow White and Seven Dwarves")
@@ -50,18 +40,40 @@ DOMSelectors.third.addEventListener("click", function idea (thrice) {
     </div>`));
 }) 
 
+ */
 function clearfields ()  {
-    DOMSelectors.box.value = "" ;
+    DOMSelectors.box.innerHTML = "" ;
 
     }
 
- */
-function filters ()
-let buttons = document.querySelectorAll ('#btn')
+function yes (arr){
+    arr.forEach((box)=> DOMSelectors.box.insertAdjacentHTML("beforeend",  `
+    <div class="card">
+    <div class ="name"> ${box.Name}</div>
+    <img src= ${box.Image} class="card-img">
+        <h3 class="card-desc"> ${box.Price}</h3>
+    </div>`));
+}
+yes(array)
+
+const buttons = document.querySelectorAll ('button');
 buttons.forEach((btn)=> btn.addEventListener("click", function () {
-    let type = btn.textContent.toLowerCase()
-    let newArr = array.filter ((array)=> array.title)
-}))
+    const type = btn.textContent.toLowerCase()
+    const newArr = array.filter ((array)=> array.Title===type);
+    clearfields();
+    yes (newArr)
+;}))
+
+DOMSelectors.change.addEventListener("click", function (event) {
+    event.preventDefault();
+ if (document.body.classList.contains("good")){
+        document.body.classList.add("bad")
+        document.body.classList.remove ("good");
+     } else {
+            document.body.classList.add("good")
+            document.body.classList.remove("bad")
+        } 
+})
 
 
 
